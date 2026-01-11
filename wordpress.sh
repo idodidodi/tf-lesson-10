cf47f881b0f0dffb1c17988ed848c75b6dfd3862
Ido Ayal <idodidodi@gmail.com>
2026-01-11 21:00:56 +0200

    Fix script

--- wordpress.sh
+++ wordpress.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+echo "Starting create wordpress docker compose"
+
 mkdir wordpress
 cd wordpress
 curl -O https://raw.githubusercontent.com/idodidodi/tf-lesson-10/main/docker-compose.yml
