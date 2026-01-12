ec38e4308bcfadc30f933a9a9cb7acefd86eff18
Ido Ayal <idodidodi@gmail.com>
2026-01-12 21:28:15 +0200

    Fix path to wordpress folder

--- wordpress.sh
+++ wordpress.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Starting create wordpress docker compose"
-
+cd /home/ubuntu
 mkdir wordpress
 cd wordpress
 curl -O https://raw.githubusercontent.com/idodidodi/tf-lesson-10/main/docker-compose.yml
-docker-compose up -d
+sudo docker-compose up -d
