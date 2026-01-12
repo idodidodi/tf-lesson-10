abde393aa6bfc6bec7b070574270d18640ea01e6
Ido Ayal <idodidodi@gmail.com>
2026-01-12 17:00:23 +0200

    Add dcker compose

--- wordpress.sh
+++ wordpress.sh
@@ -5,4 +5,4 @@ echo "Starting create wordpress docker compose"
 mkdir wordpress
 cd wordpress
 curl -O https://raw.githubusercontent.com/idodidodi/tf-lesson-10/main/docker-compose.yml
-
+docker-compose up -d
